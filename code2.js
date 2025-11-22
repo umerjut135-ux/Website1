32d0c6c67d2ed64581e7f436fa073b33ad73c954
umerjut135-ux <umerjut135@gmail.com>
2025-11-22 07:11:05 -0800

    Add files via upload

---
+++ code2.js
@@ -0,0 +1,6 @@
+// Simple JS effect for button click
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Thank you for connecting with Wedding Planner!');
+  });
+});
